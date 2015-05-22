ae0c82d88bb46ff00550ec4a248eb31df1dab731
Mathieu Méa <mathieu.mea@gmail.com>
2015-05-22 09:02:57 -0400

    Compatibility with shared parser library & code cleaning.

--- src/org/mtransit/parser/ca_quebec_orleans_express_bus/QuebecOrleansExpressBusAgencyTools.java
+++ src/org/mtransit/parser/ca_quebec_orleans_express_bus/QuebecOrleansExpressBusAgencyTools.java
@@ -8,6 +8,7 @@ import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
+import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MRoute;
@@ -76,25 +77,43 @@ public class QuebecOrleansExpressBusAgencyTools extends DefaultAgencyTools {
 		return MSpec.cleanLabel(routeLongName);
 	}
 
+	private static final String RID_123 = "123";
+	private static final String RID_122 = "122";
+	private static final String RID_11 = "11";
+	private static final String RID_9 = "9";
+	private static final String RID_8 = "8";
+	private static final String RID_6 = "6";
+	private static final String RID_4 = "4";
+	private static final String RID_2 = "2";
+
+	private static final String RSN_123 = "QC RK";
+	private static final String RSN_122 = "MT RK E";
+	private static final String RSN_11 = "VT MT";
+	private static final String RSN_9 = "RK GR S";
+	private static final String RSN_8 = "RK GS N";
+	private static final String RSN_6 = "MT QC N";
+	private static final String RSN_4 = "MT QC S";
+	private static final String RSN_2 = "MT YUL";
+
 	@Override
 	public String getRouteShortName(GRoute gRoute) {
 		if (StringUtils.isEmpty(gRoute.route_short_name)) {
-			if ("2".equals(gRoute.route_id)) {
-				return "MT YUL"; // Montréal - Aéroport Montréal-Trudeau
-			} else if ("4".equals(gRoute.route_id)) {
-				return "MT QC S"; // Montréal - Québec ( Rive-sud, Express )
-			} else if ("6".equals(gRoute.route_id)) {
-				return "MT QC N"; // Montréal - Trois-Rivières - Québec ( Rive Nord )
-			} else if ("8".equals(gRoute.route_id)) {
-				return "RK GS N"; // Rimouski - Matane - Gaspé ( Gaspésie Nord )
-			} else if ("9".equals(gRoute.route_id)) {
-				return "RK GR S"; // Rimouski - Grande-Rivière (Gaspésie Sud)
-			} else if ("11".equals(gRoute.route_id)) {
-				return "VT MT"; // Victoriaville - Montréal
-			} else if ("122".equals(gRoute.route_id)) {
-				return "MT RK E"; // 'Montréal - Rimouski (Express)
-			} else if ("123".equals(gRoute.route_id)) {
-				return "QC RK"; // Québec - Rimouski (Bas-St-Laurent)
+			if (RID_2.equals(gRoute.route_id)) {
+				return RSN_2; // Montréal - Aéroport Montréal-Trudeau
+			} else if (RID_4.equals(gRoute.route_id)) {
+				return RSN_4; // Montréal - Québec ( Rive-sud, Express )
+			} else if (RID_6.equals(gRoute.route_id)) {
+				return RSN_6; // Montréal - Trois-Rivières - Québec ( Rive Nord )
+			} else if (RID_8.equals(gRoute.route_id)) {
+				return RSN_8; // Rimouski - Matane - Gaspé ( Gaspésie Nord )
+			} else if (RID_9.equals(gRoute.route_id)) {
+				return RSN_9; // Rimouski - Grande-Rivière (Gaspésie Sud)
+			} else if (RID_11.equals(gRoute.route_id)) {
+				return RSN_11; // Victoriaville - Montréal
+			} else if (RID_122.equals(gRoute.route_id)) {
+				return RSN_122; // 'Montréal - Rimouski (Express)
+			} else if (RID_123.equals(gRoute.route_id)) {
+				return RSN_123; // Québec - Rimouski (Bas-St-Laurent)
 			}
 		}
 		return super.getRouteShortName(gRoute);
@@ -107,81 +126,93 @@ public class QuebecOrleansExpressBusAgencyTools extends DefaultAgencyTools {
 		return AGENCY_COLOR;
 	}
 
+	private static final String COLOR_ED253F = "ED253F";
+	private static final String COLOR_0F807C = "0F807C";
+	private static final String COLOR_75B442 = "75B442";
+	private static final String COLOR_EB6B24 = "EB6B24";
+	private static final String COLOR_2B80A5 = "2B80A5";
+
 	@Override
 	public String getRouteColor(GRoute gRoute) {
-		if ("2".equals(gRoute.route_id)) return "2B80A5";
-		if ("4".equals(gRoute.route_id)) return "2B80A5";
-		if ("6".equals(gRoute.route_id)) return "EB6B24";
-		if ("8".equals(gRoute.route_id)) return "75B442";
-		if ("9".equals(gRoute.route_id)) return "75B442";
-		if ("11".equals(gRoute.route_id)) return "0F807C";
-		if ("122".equals(gRoute.route_id)) return "ED253F";
-		if ("123".equals(gRoute.route_id)) return "ED253F";
+		if (RID_2.equals(gRoute.route_id)) return COLOR_2B80A5;
+		if (RID_4.equals(gRoute.route_id)) return COLOR_2B80A5;
+		if (RID_6.equals(gRoute.route_id)) return COLOR_EB6B24;
+		if (RID_8.equals(gRoute.route_id)) return COLOR_75B442;
+		if (RID_9.equals(gRoute.route_id)) return COLOR_75B442;
+		if (RID_11.equals(gRoute.route_id)) return COLOR_0F807C;
+		if (RID_122.equals(gRoute.route_id)) return COLOR_ED253F;
+		if (RID_123.equals(gRoute.route_id)) return COLOR_ED253F;
 		return super.getRouteColor(gRoute);
 	}
+
+	private static final String MONTRÉAL = "Montréal";
+	private static final String RIMOUSKI = "Rimouski";
+	private static final String QUÉBEC = "Québec";
+	private static final String AÉROPORT_TRUDEAU = "Aéroport-Trudeau";
+	private static final String TRUDEAU = "Trudeau";
 
 	private static final int INBOUND = 0;
 
 	private static final int OUTBOUND = 1;
 
 	@Override
-	public void setTripHeadsign(MRoute route, MTrip mTrip, GTrip gTrip) {
+	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
 		String stationName = cleanTripHeadsign(gTrip.trip_headsign);
 		int directionId;
 		int routeId = Integer.parseInt(gTrip.getRouteId());
 		switch (routeId) {
 		case 2:
-			if (gTrip.trip_headsign.contains("Trudeau")) {
-				stationName = "Aéroport-Trudeau";
+			if (gTrip.trip_headsign.contains(TRUDEAU)) {
+				stationName = AÉROPORT_TRUDEAU;
 				directionId = OUTBOUND;
 			} else {
 				directionId = INBOUND;
 			}
 			break;
 		case 4:
-			if ("Québec".equals(gTrip.trip_headsign)) {
+			if (QUÉBEC.equals(gTrip.trip_headsign)) {
 				directionId = OUTBOUND;
 			} else {
 				directionId = INBOUND;
 			}
 			break;
 		case 6:
-			if ("Québec".equals(gTrip.trip_headsign)) {
+			if (QUÉBEC.equals(gTrip.trip_headsign)) {
 				directionId = OUTBOUND;
 			} else {
 				directionId = INBOUND;
 			}
 			break;
 		case 8:
-			if ("Rimouski".equals(gTrip.trip_headsign)) {
+			if (RIMOUSKI.equals(gTrip.trip_headsign)) {
 				directionId = INBOUND;
 			} else {
 				directionId = OUTBOUND;
 			}
 			break;
 		case 9:
-			if ("Rimouski".equals(gTrip.trip_headsign)) {
+			if (RIMOUSKI.equals(gTrip.trip_headsign)) {
 				directionId = INBOUND;
 			} else {
 				directionId = OUTBOUND;
 			}
 			break;
 		case 11:
-			if ("Montréal".equals(gTrip.trip_headsign)) {
+			if (MONTRÉAL.equals(gTrip.trip_headsign)) {
 				directionId = INBOUND;
 			} else {
 				directionId = OUTBOUND;
 			}
 			break;
 		case 122:
-			if ("Montréal".equals(gTrip.trip_headsign)) {
+			if (MONTRÉAL.equals(gTrip.trip_headsign)) {
 				directionId = INBOUND;
 			} else {
 				directionId = OUTBOUND;
 			}
 			break;
 		case 123:
-			if ("Québec".equals(gTrip.trip_headsign)) {
+			if (QUÉBEC.equals(gTrip.trip_headsign)) {
 				directionId = INBOUND;
 			} else {
 				directionId = OUTBOUND;
@@ -200,8 +231,4 @@ public class QuebecOrleansExpressBusAgencyTools extends DefaultAgencyTools {
 		return MSpec.cleanLabelFR(tripHeadsign);
 	}
 
-	@Override
-	public String cleanStopName(String gStopName) {
-		return super.cleanStopNameFR(gStopName);
-	}
 }
